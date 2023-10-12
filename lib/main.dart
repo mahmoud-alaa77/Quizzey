e01715afa36748baa57711b3c38a9cde0215a04c
MAHMOUD <mahmoud-alaa77@gmail.com>
2023-10-12 15:10:26 +0300

    finish v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:untitled2/cubits/login_cubit/login_cubit.dart';
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
 
       ],
       child: MaterialApp(
-        builder: DevicePreview.appBuilder,
+
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
